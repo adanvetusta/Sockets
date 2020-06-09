c4450972018048a52d96bacafacd4fe59a143978
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-06-09 13:18:50 +0200

    Patrón singleton: instancia

--- 01-server/clases/server.ts
+++ 01-server/clases/server.ts
@@ -5,17 +5,36 @@ import http from 'http';
 
 export default class Server {
     
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
 
     public io: socketIO.Server;
     private httpServer: http.Server;
 
-    constructor() {
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
         this.httpServer = new http.Server(this.app);
         this.io = socketIO(this.httpServer);
+
+        this.escucharSockets();
+    }
+
+
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+
+    private escucharSockets() {
+        console.log('Escuchando conexiones - sockets');
+
+        // On: Escuchar algún evento.
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+        });
     }
 
     start(callback: () => void) {
--- 01-server/index.ts
+++ 01-server/index.ts
@@ -1,9 +1,9 @@
-import Server from "./clases/server";
+import Server from './clases/server';
 import { router } from "./routes/router";
 import  bodyParser from "body-parser";
 import cors from 'cors';
 
-const server = new Server();
+const server = Server.instance;
 server.app.use(bodyParser.urlencoded({extended: true}));
 server.app.use(bodyParser.json());
 
