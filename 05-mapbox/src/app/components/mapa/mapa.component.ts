f1f996dd2dc4951a340e88969b289478f75b4cb3
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-06-15 22:47:56 +0200

    Mostrar mapa [Mapbox]
    Dependencias de desarrollo:
    - npm install --save-dev  @types/mapbox-gl

--- 05-mapbox/src/app/components/mapa/mapa.component.ts
+++ 05-mapbox/src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mapa',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  mapa: mapboxgl.Map;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.crearMapa();
+  }
+
+  crearMapa() {
+    (mapboxgl as any).accessToken = 'pk.eyJ1IjoiYWRhbnZldHVzdGEiLCJhIjoiY2tiZ3R0aml4MTh0ejMxcDhlZzZpd25maiJ9.V1mwfJitXF25Ps2qbrfz7Q';
+    this.mapa = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-75.75512993582937, 45.349977429009954],
+      zoom: 15.8
+    });
   }
 
 }
