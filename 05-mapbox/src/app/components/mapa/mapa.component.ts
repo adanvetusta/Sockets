3e608aacb558203c5dfc305eea3a3a648c1ee3ec
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-06-16 17:52:20 +0200

    Mostrado de marcadores en el mapa

--- 05-mapbox/src/app/components/mapa/mapa.component.ts
+++ 05-mapbox/src/app/components/mapa/mapa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import { Lugar } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-mapa',
@@ -10,6 +11,28 @@ export class MapaComponent implements OnInit {
 
   mapa: mapboxgl.Map;
 
+  lugares: Lugar[] = [{
+    id: '1',
+    nombre: 'Fernando',
+    lng: -75.75512993582937,
+    lat: 45.349977429009954,
+    color: '#dd8fee'
+  },
+  {
+    id: '2',
+    nombre: 'Amy',
+    lng: -75.75195645527508,
+    lat: 45.351584045823756,
+    color: '#790af0'
+  },
+  {
+    id: '3',
+    nombre: 'Orlando',
+    lng: -75.75900589557777,
+    lat: 45.34794635758547,
+    color: '#19884b'
+  }];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,13 +40,36 @@ export class MapaComponent implements OnInit {
   }
 
   crearMapa() {
-    (mapboxgl as any).accessToken = 'pk.eyJ1IjoiYWRhbnZldHVzdGEiLCJhIjoiY2tiZ3R0aml4MTh0ejMxcDhlZzZpd25maiJ9.V1mwfJitXF25Ps2qbrfz7Q';
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhbnZldHVzdGEiLCJhIjoiY2tiZ3R0aml4MTh0ejMxcDhlZzZpd25maiJ9.V1mwfJitXF25Ps2qbrfz7Q';
+
     this.mapa = new mapboxgl.Map({
       container: 'mapa',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-75.75512993582937, 45.349977429009954],
       zoom: 15.8
     });
+    for (const marcador of this.lugares) {
+      this.agregarMarcador(marcador);
+    }
   }
 
+  agregarMarcador(marcador: Lugar) {
+    const html = `<h2>${ marcador.nombre }</h2>
+                  <br>
+                  <button>Borrar</button>`;
+
+    const customPoup = new mapboxgl.Popup({
+      offset: 25,
+      closeOnClick: false
+    }).setHTML(html);
+
+    const marker = new mapboxgl.Marker({
+      draggable: false,
+      color: marcador.color,
+      style: 'mapbox://styles/mapbox/streets-v11'
+    })
+    .setPopup(customPoup)
+    .setLngLat([marcador.lng, marcador.lat])
+    .addTo(this.mapa);
+  }
 }
