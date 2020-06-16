5d50cdf06d628e111b0fe692258b134f4311a479
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-06-16 19:21:54 +0200

    Eliminar marcadores

--- 05-mapbox/src/app/components/mapa/mapa.component.ts
+++ 05-mapbox/src/app/components/mapa/mapa.component.ts
@@ -55,14 +55,22 @@ export class MapaComponent implements OnInit {
   }
 
   agregarMarcador(marcador: Lugar) {
-    const html = `<h2>${ marcador.nombre }</h2>
-                  <br>
-                  <button>Borrar</button>`;
+
+    const h2 = document.createElement('h2');
+    h2.innerText = marcador.nombre;
+
+    const btnBorrar = document.createElement('button');
+    btnBorrar.innerText = 'Borrar';
+
+    const div = document.createElement('div');
+    div.append(h2, btnBorrar);
+
+
 
     const customPoup = new mapboxgl.Popup({
       offset: 25,
       closeOnClick: false
-    }).setHTML(html);
+    }).setDOMContent(div); 
 
     const marker = new mapboxgl.Marker({
       draggable: true,
@@ -79,6 +87,12 @@ export class MapaComponent implements OnInit {
       console.log(lngLat);
 
       // TO DO: crear evento para emitir las coordenadas de este marcador
+    });
+
+    btnBorrar.addEventListener('click', () => {
+      marker.remove();
+
+      // TO DO: eliminar el marcador mediante sockets
     });
   }
 
